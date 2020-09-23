61a01a83bf15f2a58a7b6fe593d8d405314ba586
Rafael <rafareinert94@hotmail.com>
2020-09-23 16:33:41 -0300

    Criação das classes.

---
+++ ProdutivedadeBasquete/ProdutivedadeBasquete/Model/Jogadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutivedadeBasquete.Model
+{
+    class Jogadora
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string CidadeNascimento { get; set; }
+        public int RecordeDePontos { get; set; }
+    }
+}
---
+++ ProdutivedadeBasquete/ProdutivedadeBasquete/Model/Jogos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutivedadeBasquete.Model
+{
+    public class Jogos
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdJogadora { get; set; }
+        public int NumJogo { get; set; }
+        public int Placar { get; set; }
+        public int QtidadeMinimaPontos { get; set; }
+        public int QtidadeMaximaPontos { get; set; }
+        public int RecordeMinimoPontos { get; set; }
+        public int RecordeMaximoPontos { get; set; }
+
+
+    }
+}
